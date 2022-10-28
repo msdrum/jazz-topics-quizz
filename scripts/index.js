93acd7d9cd073e3411ccee0524d547d88e8945c0
Marcio Silva <marciosilvadrum@gmail.com>
2022-10-28 19:21:13 -0300

    adding first button functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,21 @@
 // DOM MANIPULATION
+
+//intanciando a classe
+const game = new JazzTopics();
+
+//capturando os elementos HTML
+const screenContainer = document.getElementById("sc-container");
+const inputName = document.getElementById("input-name");
+const btnStart = document.getElementById("btn-start");
+
+//add event Listner no botão JOGAR
+btnStart.addEventListener("click", () => {
+  //hidding start screen
+  screenContainer.classList.add("hide");
+
+  //mostrar a segunda tela (quiz screen)
+
+  //pegando o nome do jogador
+  game.player = inputName.value; //enviando o nome do jogador para a class
+  //console.log(inputName.value);
+});
