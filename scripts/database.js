e25c35e13f99938d2525661c000821ce8e87e5e5
Marcio Silva <marciosilvadrum@gmail.com>
2022-10-30 15:12:00 -0300

    add the database quiz questions

--- scripts/database.js
+++ scripts/database.js
@@ -1,62 +1,164 @@
 // QUESTIONS DATABASE
 
 // exports.allQuestions;
-const allQuestions = [
-  {
-    question: "Primeira pergunta teste!",
-    awnser: "Primeira resposta correta!",
-    options: ["resposta a", "resposta b", "resposta c", "resposta d"],
-  },
-  {
-    question: "Segunda pergunta teste!",
-    awnser: "Segunda resposta correta!",
-    options: ["resposta a", "resposta b", "resposta c", "resposta d"],
-  },
-  {
-    question: "Terceira pergunta teste!",
-    awnser: "Terceira resposta correta!",
-    options: ["resposta a", "resposta b", "resposta c", "resposta d"],
-  },
-  {
-    question: "Quarta pergunta teste!",
-    awnser: "Quarta resposta correta!",
-    options: ["resposta a", "resposta b", "resposta c", "resposta d"],
-  },
-  {
-    question: "Quinta pergunta teste!",
-    awnser: "Quinta resposta correta!",
-    options: ["resposta a", "resposta b", "resposta c", "resposta d"],
-  },
-  {
-    question: "Sexta pergunta teste!",
-    awnser: "Sexta resposta correta!",
-    options: ["resposta a", "resposta b", "resposta c", "resposta d"],
-  },
-  {
-    question: "Sétima pergunta teste!",
-    awnser: "Sétima resposta correta!",
-    options: ["resposta a", "resposta b", "resposta c", "resposta d"],
-  },
-  {
-    question: "Oitava pergunta teste!",
-    awnser: "Oitava resposta correta!",
-    options: ["resposta a", "resposta b", "resposta c", "resposta d"],
-  },
-  {
-    question: "Nona pergunta teste!",
-    awnser: "RNona resposta correta!",
-    options: ["resposta a", "resposta b", "resposta c", "resposta d"],
-  },
-  {
-    question: "Décima pergunta teste!",
-    awnser: "Décima resposta correta!",
-    options: ["resposta a", "resposta b", "resposta c", "resposta d"],
-  },
-  {
-    question: "Décima primeira pergunta teste!",
-    awnser: "Décima primeira resposta correta!",
-    options: ["resposta a", "resposta b", "resposta c", "resposta d"],
-  },
-];
+// const allQuestions = [
+//   {
+//     question: "Primeira pergunta teste!",
+//     awnser: "Primeira resposta correta!",
+//     options: ["resposta a", "resposta b", "resposta c", "resposta d"],
+//   },
+//   {
+//     question: "Segunda pergunta teste!",
+//     awnser: "Segunda resposta correta!",
+//     options: ["resposta a", "resposta b", "resposta c", "resposta d"],
+//   },
+//   {
+//     question: "Terceira pergunta teste!",
+//     awnser: "Terceira resposta correta!",
+//     options: ["resposta a", "resposta b", "resposta c", "resposta d"],
+//   },
+//   {
+//     question: "Quarta pergunta teste!",
+//     awnser: "Quarta resposta correta!",
+//     options: ["resposta a", "resposta b", "resposta c", "resposta d"],
+//   },
+//   {
+//     question: "Quinta pergunta teste!",
+//     awnser: "Quinta resposta correta!",
+//     options: ["resposta a", "resposta b", "resposta c", "resposta d"],
+//   },
+//   {
+//     question: "Sexta pergunta teste!",
+//     awnser: "Sexta resposta correta!",
+//     options: ["resposta a", "resposta b", "resposta c", "resposta d"],
+//   },
+//   {
+//     question: "Sétima pergunta teste!",
+//     awnser: "Sétima resposta correta!",
+//     options: ["resposta a", "resposta b", "resposta c", "resposta d"],
+//   },
+//   {
+//     question: "Oitava pergunta teste!",
+//     awnser: "Oitava resposta correta!",
+//     options: ["resposta a", "resposta b", "resposta c", "resposta d"],
+//   },
+//   {
+//     question: "Nona pergunta teste!",
+//     awnser: "RNona resposta correta!",
+//     options: ["resposta a", "resposta b", "resposta c", "resposta d"],
+//   },
+//   {
+//     question: "Décima pergunta teste!",
+//     awnser: "Décima resposta correta!",
+//     options: ["resposta a", "resposta b", "resposta c", "resposta d"],
+//   },
+//   {
+//     question: "Décima primeira pergunta teste!",
+//     awnser: "Décima primeira resposta correta!",
+//     options: ["resposta a", "resposta b", "resposta c", "resposta d"],
+//   },
+// ];
 
 //export { allQuestions };
+
+const jazzQuestions = [
+  {
+    question:
+      "Em 1917 foi gravado o primeiro disco de jazz da história, pela gravadora Victor. Qual era o nome da banda que gravou esse disco?",
+    awnser: "The Orginal Dixieland Jazz Band",
+    options: [
+      "The Orginal Dixieland Jazz Band",
+      "The Original Creole Orchestra",
+      "Louis Armstrong and his Hot Seven",
+      "The Original Jazz Dixeland Orchestra",
+    ],
+  },
+  {
+    question:
+      "Qual apelido Louis Arsmotrong recebeu, ainda na infância, e carregou durante toda a sua vida?",
+    awnser: "Satchmo",
+    options: ["Satchmo", "Scat Man", "Blues Man", "King of Swing"],
+  },
+  {
+    question:
+      "Em 1923, após assitir ao concerto de um grande músico em Washington, Duke Ellington resolveu ir morar em Nova Iorque e tentar a vida como músico nesta cidade. Qual era o nome desse músico?",
+    awnser: "Sidney Bechet",
+    options: [
+      "Sidney Bechet",
+      "Louis Armstrong",
+      "Benny Goodman",
+      "King Oliver",
+    ],
+  },
+  {
+    question:
+      "Louis Armstrong influenciou a música americana de diversas formas, uma delas foi a maneira que ele desenvolveu de cantar melodias, improvisando como se estivesse tocando o trompete. Qual o nome dado a esta técnica?",
+    awnser: "Scat Singing",
+    options: [
+      "Scat Singing",
+      "Melodine Singing",
+      "Satchmo Singing",
+      "Vocalize Singing",
+    ],
+  },
+  {
+    question:
+      "O advento da Lei Sêca no EUA trouxe um grande mercado paralelo de clubes clandestinos, e dentre esses clubes havia um no Harlem administrado por gângsters, que viria a abrigar a orquestra de Duke Ellington por muito tempo. No final de 1927, a rádio CBS passou a transmitir as apresentações por todo os EUA. Qual era o nome desse clube? ",
+    awnser: "Cotton Club",
+    options: [
+      "Cotton Club",
+      "Roseland Ballroom",
+      "Savoy Ballroom",
+      "Palomar Club",
+    ],
+  },
+  {
+    question: "Qual a estrutura harmônica básica de um blues de 12 compassos?",
+    awnser: "I7, IV7 e V7",
+    options: ["I7, IV7 e V7", "I, IV7 e V7", "I7, IV7 e V", "I, IV e V7"],
+  },
+  {
+    question:
+      "Louis Armstrong foi considerado o primeiro grande solista do jazz e isto se deve à sua maneira de construir o discurso de seu improviso baseado na ideia de construção de histórias. Ele e o pianista de seu grupo Earl Hines ficaram imortalizados por uma música gravada em 1928, chamada: ",
+    awnser: "West End Blues",
+    options: [
+      "West End Blues",
+      "Hines Blues",
+      "What a Wonderful World",
+      "Summertime",
+    ],
+  },
+  {
+    question:
+      "Na década de 20, havia uma cantora que foi considerada a maior catora de blues do seu tempo.Ela possuia tempramento forte, viajava em seu próprio vagão de trem e trabalhou em um dos primeiros filmes sonoros que tinham em seu elenco atores negros.",
+    awnser: "Bessie Smith",
+    options: [
+      "Bessie Smith",
+      "Billie Holiday",
+      "Aretha Franklin",
+      "Ella Fitzgerald",
+    ],
+  },
+  {
+    question:
+      "A década de 30 trouxe consigo um novo estilo de música feita para as big bands, ele se chamava Swing. O estilo teve vários representantes expressivos, mas um deles recebeu o apelido de 'O Rei do Swing'. Qual era o seu nome?",
+    awnser: "Benny Goodman",
+    options: [
+      "Benny Goodman",
+      "Duke Ellington",
+      "Paul Whiteman",
+      "Fletcher Henderson",
+    ],
+  },
+  {
+    question:
+      "Take The 'A' Train é um grande clássico do jazz e foi composta por um dos maiores compositores da música americana, seu nome era:",
+    awnser: "Duke Ellington",
+    options: [
+      "Duke Ellington",
+      "Benny Goodman",
+      "Coleman Hawkins",
+      "Louis Armstrong",
+    ],
+  },
+];
