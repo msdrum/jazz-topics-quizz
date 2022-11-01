ff12e5043d6809df2ab2d2bf5f6eda23033ab74d
Marcio Silva <marciosilvadrum@gmail.com>
2022-11-01 15:43:49 -0300

    fixing database bug on class file

--- scripts/JazzTopics.js
+++ scripts/JazzTopics.js
@@ -6,109 +6,14 @@ class JazzTopics {
   constructor() {
     this.player = "";
     this.questions = [
-      // {
-      //   question:
-      //     "Em 1917 foi gravado o primeiro disco de jazz da história.O disco foi lançado pela gravadora Victor. Qual era o nome da banda que gravou esse disco?",
-      //   awnser: "The Orginal Dixieland Jazz Band",
-      //   options: [
-      //     "The Orginal Dixieland Jazz Band",
-      //     "The Original Creole Orchestra",
-      //     "Louis Armstrong and his Hot Seven",
-      //     "The Original Jazz Dixeland Orchestra",
-      //   ],
-      // },
-      // {
-      //   question:
-      //     "Qual apelido Louis Arsmotrong recebeu, ainda na infância, e carregou durante toda a sua vida?",
-      //   awnser: "Satchmo",
-      //   options: ["Satchmo", "Scat Man", "Blues Man", "King of Swing"],
-      // },
-      // {
-      //   question:
-      //     "Em 1923, após assitir o concerto de um grande músico em Washington, Duke Ellington resolveu ir morar em Nova Iorque e tentar a vida como músico nesta cidade. Qual era o nome desse músico?",
-      //   awnser: "Sidney Bechet",
-      //   options: [
-      //     "Sidney Bechet",
-      //     "Louis Armstrong",
-      //     "Benny Goodman",
-      //     "King Oliver",
-      //   ],
-      // },
-      // {
-      //   question:
-      //     "Louis Armstrong influenciou a música americana de diversas formas, uma delas foi a maneira que ele desenvolveu de cantar melodias, improvisando como se estivesse tocando o trompete. Qual o nome dado a esta técnica?",
-      //   awnser: "Scat Singing",
-      //   options: [
-      //     "Scat Singing",
-      //     "Melodine Singing",
-      //     "Satchmo Singing",
-      //     "Vocalize Singing",
-      //   ],
-      // },
-      // {
-      //   question:
-      //     "O advento da Lei Sêca no EUA trouxe um grande mercado paralelo de clubes clandestinos, dentre esses clubes, havia um no Harlem administrado por gângsters que viria a abrigar a orquestra de Duke Ellington por muito tempo. No final de 1927, a rádio CBS passou a transmitir as apresentações ocorridas no local para todo os EUA. Qual era o nome desse clube? ",
-      //   awnser: "Cotton Club",
-      //   options: ["Cotton Club", "Roseland Club", "Savoy Club", "Palomar Club"],
-      // },
-      // {
-      //   question:
-      //     "Qual a estrutura harmônica básica de um blues de 12 compassos?",
-      //   awnser: "I7, IV7 e V7",
-      //   options: ["I7, IV7 e V7", "I, IV7 e V7", "I7, IV7 e V", "I, IV e V7"],
-      // },
-      // {
-      //   question:
-      //     "Louis Armstrong foi considerado o primeiro grande solista do jazz e isto se deve à sua maneira de construir o discurso de seu improviso baseado na ideia de construção de histórias. Ele e o pianista de seu grupo Earl Hines ficaram imortalizados por uma música gravada em 1928, chamada: ",
-      //   awnser: "West End Blues",
-      //   options: [
-      //     "West End Blues",
-      //     "Hines Blues",
-      //     "What a Wonderful World",
-      //     "Summertime",
-      //   ],
-      // },
-      // {
-      //   question:
-      //     "Na década de 20, havia uma cantora que foi considerada a maior catora de blues do seu tempo.Ela possuia tempramento forte, viajava em seu próprio vagão de trem e trabalhou em um dos primeiros filmes sonoros que tinham em seu elenco atores negros.",
-      //   awnser: "Bessie Smith",
-      //   options: [
-      //     "Bessie Smith",
-      //     "Billie Holiday",
-      //     "Aretha Franklin",
-      //     "Ella Fitzgerald",
-      //   ],
-      // },
-      // {
-      //   question:
-      //     "A década de 30 trouxe consigo um novo estilo de música feita para as big bands, ele se chamava Swing. O estilo teve vários representantes expressivos, mas um deles recebeu o apelido de 'O Rei do Swing'. Qual era o seu nome?",
-      //   awnser: "Benny Goodman",
-      //   options: [
-      //     "Benny Goodman",
-      //     "Duke Ellington",
-      //     "Paul Whiteman",
-      //     "Fletcher Henderson",
-      //   ],
-      // },
-      // {
-      //   question:
-      //     "Take The 'A' Train é um grande clássico do jazz e foi composta por um dos maiores compositores da música americana, seu nome era:",
-      //   awnser: "Duke Ellington",
-      //   options: [
-      //     "Duke Ellington",
-      //     "Benny Goodman",
-      //     "Coleman Hawkins",
-      //     "Louis Armstrong",
-      //   ],
-      // },
       {
         question:
           "Em 1917 foi gravado o primeiro disco de jazz da história.O disco foi lançado pela gravadora Victor. Qual era o nome da banda que gravou esse disco?",
         awnser: "The Orginal Dixieland Jazz Band",
         options: [
+          "The Orginal Dixieland Jazz Band",
           "The Original Creole Orchestra",
           "Louis Armstrong and his Hot Seven",
-          "The Orginal Dixieland Jazz Band",
           "The Original Jazz Dixeland Orchestra",
         ],
       },
@@ -116,16 +21,16 @@ class JazzTopics {
         question:
           "Qual apelido Louis Arsmotrong recebeu, ainda na infância, e carregou durante toda a sua vida?",
         awnser: "Satchmo",
-        options: ["Scat Man", "Blues Man", "King of Swing", "Satchmo"],
+        options: ["Satchmo", "Scat Man", "Blues Man", "King of Swing"],
       },
       {
         question:
           "Em 1923, após assitir o concerto de um grande músico em Washington, Duke Ellington resolveu ir morar em Nova Iorque e tentar a vida como músico nesta cidade. Qual era o nome desse músico?",
         awnser: "Sidney Bechet",
         options: [
-          "Benny Goodman",
           "Sidney Bechet",
           "Louis Armstrong",
+          "Benny Goodman",
           "King Oliver",
         ],
       },
@@ -144,22 +49,22 @@ class JazzTopics {
         question:
           "O advento da Lei Sêca no EUA trouxe um grande mercado paralelo de clubes clandestinos, dentre esses clubes, havia um no Harlem administrado por gângsters que viria a abrigar a orquestra de Duke Ellington por muito tempo. No final de 1927, a rádio CBS passou a transmitir as apresentações ocorridas no local para todo os EUA. Qual era o nome desse clube? ",
         awnser: "Cotton Club",
-        options: ["Palomar Club", "Cotton Club", "Roseland Club", "Savoy Club"],
+        options: ["Cotton Club", "Roseland Club", "Savoy Club", "Palomar Club"],
       },
       {
         question:
           "Qual a estrutura harmônica básica de um blues de 12 compassos?",
         awnser: "I7, IV7 e V7",
-        options: ["I7, IV7 e V", "I, IV7 e V7", "I7, IV7 e V7", "I, IV e V7"],
+        options: ["I7, IV7 e V7", "I, IV7 e V7", "I7, IV7 e V", "I, IV e V7"],
       },
       {
         question:
           "Louis Armstrong foi considerado o primeiro grande solista do jazz e isto se deve à sua maneira de construir o discurso de seu improviso baseado na ideia de construção de histórias. Ele e o pianista de seu grupo Earl Hines ficaram imortalizados por uma música gravada em 1928, chamada: ",
         awnser: "West End Blues",
         options: [
+          "West End Blues",
           "Hines Blues",
           "What a Wonderful World",
-          "West End Blues",
           "Summertime",
         ],
       },
@@ -168,8 +73,8 @@ class JazzTopics {
           "Na década de 20, havia uma cantora que foi considerada a maior catora de blues do seu tempo.Ela possuia tempramento forte, viajava em seu próprio vagão de trem e trabalhou em um dos primeiros filmes sonoros que tinham em seu elenco atores negros.",
         awnser: "Bessie Smith",
         options: [
-          "Billie Holiday",
           "Bessie Smith",
+          "Billie Holiday",
           "Aretha Franklin",
           "Ella Fitzgerald",
         ],
@@ -179,9 +84,9 @@ class JazzTopics {
           "A década de 30 trouxe consigo um novo estilo de música feita para as big bands, ele se chamava Swing. O estilo teve vários representantes expressivos, mas um deles recebeu o apelido de 'O Rei do Swing'. Qual era o seu nome?",
         awnser: "Benny Goodman",
         options: [
-          "Paul Whiteman",
           "Benny Goodman",
           "Duke Ellington",
+          "Paul Whiteman",
           "Fletcher Henderson",
         ],
       },
@@ -190,12 +95,107 @@ class JazzTopics {
           "Take The 'A' Train é um grande clássico do jazz e foi composta por um dos maiores compositores da música americana, seu nome era:",
         awnser: "Duke Ellington",
         options: [
+          "Duke Ellington",
           "Benny Goodman",
           "Coleman Hawkins",
           "Louis Armstrong",
-          "Duke Ellington",
         ],
       },
+      // {
+      //   question:
+      //     "Em 1917 foi gravado o primeiro disco de jazz da história.O disco foi lançado pela gravadora Victor. Qual era o nome da banda que gravou esse disco?",
+      //   awnser: "The Orginal Dixieland Jazz Band",
+      //   options: [
+      //     "The Original Creole Orchestra",
+      //     "Louis Armstrong and his Hot Seven",
+      //     "The Orginal Dixieland Jazz Band",
+      //     "The Original Jazz Dixeland Orchestra",
+      //   ],
+      // },
+      // {
+      //   question:
+      //     "Qual apelido Louis Arsmotrong recebeu, ainda na infância, e carregou durante toda a sua vida?",
+      //   awnser: "Satchmo",
+      //   options: ["Scat Man", "Blues Man", "King of Swing", "Satchmo"],
+      // },
+      // {
+      //   question:
+      //     "Em 1923, após assitir o concerto de um grande músico em Washington, Duke Ellington resolveu ir morar em Nova Iorque e tentar a vida como músico nesta cidade. Qual era o nome desse músico?",
+      //   awnser: "Sidney Bechet",
+      //   options: [
+      //     "Benny Goodman",
+      //     "Sidney Bechet",
+      //     "Louis Armstrong",
+      //     "King Oliver",
+      //   ],
+      // },
+      // {
+      //   question:
+      //     "Louis Armstrong influenciou a música americana de diversas formas, uma delas foi a maneira que ele desenvolveu de cantar melodias, improvisando como se estivesse tocando o trompete. Qual o nome dado a esta técnica?",
+      //   awnser: "Scat Singing",
+      //   options: [
+      //     "Scat Singing",
+      //     "Melodine Singing",
+      //     "Satchmo Singing",
+      //     "Vocalize Singing",
+      //   ],
+      // },
+      // {
+      //   question:
+      //     "O advento da Lei Sêca no EUA trouxe um grande mercado paralelo de clubes clandestinos, dentre esses clubes, havia um no Harlem administrado por gângsters que viria a abrigar a orquestra de Duke Ellington por muito tempo. No final de 1927, a rádio CBS passou a transmitir as apresentações ocorridas no local para todo os EUA. Qual era o nome desse clube? ",
+      //   awnser: "Cotton Club",
+      //   options: ["Palomar Club", "Cotton Club", "Roseland Club", "Savoy Club"],
+      // },
+      // {
+      //   question:
+      //     "Qual a estrutura harmônica básica de um blues de 12 compassos?",
+      //   awnser: "I7, IV7 e V7",
+      //   options: ["I7, IV7 e V", "I, IV7 e V7", "I7, IV7 e V7", "I, IV e V7"],
+      // },
+      // {
+      //   question:
+      //     "Louis Armstrong foi considerado o primeiro grande solista do jazz e isto se deve à sua maneira de construir o discurso de seu improviso baseado na ideia de construção de histórias. Ele e o pianista de seu grupo Earl Hines ficaram imortalizados por uma música gravada em 1928, chamada: ",
+      //   awnser: "West End Blues",
+      //   options: [
+      //     "Hines Blues",
+      //     "What a Wonderful World",
+      //     "West End Blues",
+      //     "Summertime",
+      //   ],
+      // },
+      // {
+      //   question:
+      //     "Na década de 20, havia uma cantora que foi considerada a maior catora de blues do seu tempo.Ela possuia tempramento forte, viajava em seu próprio vagão de trem e trabalhou em um dos primeiros filmes sonoros que tinham em seu elenco atores negros.",
+      //   awnser: "Bessie Smith",
+      //   options: [
+      //     "Billie Holiday",
+      //     "Bessie Smith",
+      //     "Aretha Franklin",
+      //     "Ella Fitzgerald",
+      //   ],
+      // },
+      // {
+      //   question:
+      //     "A década de 30 trouxe consigo um novo estilo de música feita para as big bands, ele se chamava Swing. O estilo teve vários representantes expressivos, mas um deles recebeu o apelido de 'O Rei do Swing'. Qual era o seu nome?",
+      //   awnser: "Benny Goodman",
+      //   options: [
+      //     "Paul Whiteman",
+      //     "Benny Goodman",
+      //     "Duke Ellington",
+      //     "Fletcher Henderson",
+      //   ],
+      // },
+      // {
+      //   question:
+      //     "Take The 'A' Train é um grande clássico do jazz e foi composta por um dos maiores compositores da música americana, seu nome era:",
+      //   awnser: "Duke Ellington",
+      //   options: [
+      //     "Benny Goodman",
+      //     "Coleman Hawkins",
+      //     "Louis Armstrong",
+      //     "Duke Ellington",
+      //   ],
+      // },
     ];
     this.round = 0;
     this.questChosen = [];
